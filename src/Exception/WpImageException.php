3dd16880579f1ac410b1bb7960fa8d586d8bfcef
Dimitri BOUTEILLE <34821762+dimitriBouteille@users.noreply.github.com>
2020-03-31 21:15:13 +0200

    Initial commit

---
+++ src/Exception/WpImageException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Dbout\WpImage;
+
+/**
+ * Class WpImageException
+ * @package Dbout\WpImage
+ *
+ * @author      Dimitri BOUTEILLE <bonjour@dimitri-bouteille.fr>
+ * @link        https://github.com/dimitriBouteille Github
+ * @copyright   (c) 2020 Dimitri BOUTEILLE
+ */
+class WpImageException extends \Exception
+{
+
+}
